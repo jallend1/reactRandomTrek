9c6f52f5014c2bc55135cc2816aa7d79037387a5
jallend1 <jallend1@gmail.com>
2020-05-10 16:17:37 -0700

    Establishes basic framework and beginnings of API call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Movie from './Movie';
+const API_KEY = '';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let results;
+
+class App extends Component {
+  state = {
+    movie: 'Star Trek Beyond'
+  }
+  getNewMovie(){
+    const trekMovies = fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=Star+Trek`)
+      .then(result => result.json())
+      .then(data => console.log(data))
+      
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <p>Random Star Trek Movie Generator</p>
+        <Movie name= { this.state.movie }/>
+        <button onClick={ this.getNewMovie }>Get New Movie</button>
+      </div>
+    );
+  }
 }
 
 export default App;
