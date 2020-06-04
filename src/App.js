56cb140e31a5e27e1b2ac42a7aae86655f141260
jallend1 <jallend1@gmail.com>
2020-06-04 15:00:44 -0700

    Factors out api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Movie from './Movie';
-const API_KEY = '';
 
 let results;
 
@@ -11,14 +10,19 @@ class App extends Component {
   getNewMovie(){
     const trekMovies = fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=Star+Trek`)
       .then(result => result.json())
-      .then(data => console.log(data))
-      
+      .then(data => {
+        const randomNumber = Math.floor(Math.random() * data.results.length);
+        console.log(data.results[randomNumber].title)
+        this.setState({
+          movie: data.results[randomNumber].title
+        });
+      })
   }
 
   render(){
     return (
       <div className="App">
-        <p>Random Star Trek Movie Generator</p>
+        <p>Random Star Trek Movie Picker</p>
         <Movie name= { this.state.movie }/>
         <button onClick={ this.getNewMovie }>Get New Movie</button>
       </div>
